88718510f5aeaa5bf184345889bbe9cfb5e21a80
Mudia Geo <mudiageo@gmail.com>
2025-05-20 16:20:06 +0000

    Update utils.ts with new types

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,21 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-
+ 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+ return twMerge(clsx(inputs));
 }
+ 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChildren<T> = T extends { children?: any }
+ ? Omit<T, "children">
+ : T;
+export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & {
+ ref?: U | null;
+};
+
 /**
   * Returns code with curly braces and backticks replaced by HTML entity equivalents
   * @param {string} html - highlighted HTML
