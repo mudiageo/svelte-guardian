f320e35dfd8f0dc6641fea28077ae0f58979d08c
Mudia Geo <mudiageo@gmail.com>
2025-07-07 20:33:05 +0000

    handle routes ending with .md

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,4 +1,21 @@
 import { sequence } from '@sveltejs/kit/hooks';
 import { handle as authHandle, middleware } from '$lib/auth';
+import { redirect } from '@sveltejs/kit';
 
-export const handle = sequence(authHandle, middleware);
+// Redirect URLs ending in .md 
+const mdRedirectHandle = async ({ event, resolve }) => {
+	const { url } = event;
+	 if (url.pathname.endsWith('.md')) {
+		let newPath = url.pathname.slice(0, -3);
+		
+		newPath = newPath.endsWith('/index') ? newPath.slice(0, -6) : newPath
+		
+		const redirectUrl = `${newPath}${url.search}${url.hash}`;
+		redirect(308, redirectUrl);
+	}
+
+	
+	return resolve(event);
+};
+
+export const handle = sequence(mdRedirectHandle, authHandle, middleware);
--- src/routes/docs/[...slug]/+page.ts
+++ src/routes/docs/[...slug]/+page.ts
@@ -3,11 +3,12 @@ import type { PageLoad } from './$types';
 
 const modules = import.meta.glob('../../../docs/**/*.md'); 
 
-export const load = (async ({ params }) => {
-  const modulePath = `../../../docs/${params.slug}.md`;
+export const load = (async ({ url, params }) => {
+  let slug = params.slug.endsWith('.md') ? params.slug.slice(0, -3) : params.slug
+  const modulePath = `../../../docs/${slug}.md`;
 
   const potentialModule = Object.entries(modules).find(([path, module]) => {  
-    return path === modulePath || path.match(new RegExp(`^../../../docs/${params.slug}/?index\.md$`));
+    return path === modulePath || path.match(new RegExp(`^../../../docs/${slug}/?index\.md$`));
   })?.[1]
 		
 	if (potentialModule) {
